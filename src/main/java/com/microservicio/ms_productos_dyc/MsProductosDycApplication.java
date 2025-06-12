8cc3078957435aa3e60ca7f29c5091b80540d9bf
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-06-12 19:53:35 -0400

    Creación proyecto de microservicio de productos, con java spring boot. Se agregaron dependencias en pom.xml

---
+++ src/main/java/com/microservicio/ms_productos_dyc/MsProductosDycApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.ms_productos_dyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductosDycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductosDycApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicio/ms_productos_dyc/MsProductosDycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.ms_productos_dyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductosDycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
