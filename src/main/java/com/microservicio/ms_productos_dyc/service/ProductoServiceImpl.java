3df6a34cf0c43cc7ac184bdebf104a3da10859fa
Jayirong <lemonsoft5555@gmail.com>
2025-06-23 21:33:56 -0400

    develop de tipo s de usuarios

--- src/main/java/com/microservicio/ms_productos_dyc/service/ProductoServiceImpl.java
+++ src/main/java/com/microservicio/ms_productos_dyc/service/ProductoServiceImpl.java
@@ -2,11 +2,15 @@ package com.microservicio.ms_productos_dyc.service;
 
 import com.microservicio.ms_productos_dyc.model.dto.ProductoDTO;
 import com.microservicio.ms_productos_dyc.model.entity.Producto;
+import com.microservicio.ms_productos_dyc.model.entity.TipoProducto;
 import com.microservicio.ms_productos_dyc.repository.ProductoRepository;
+import com.microservicio.ms_productos_dyc.repository.TipoProductoRepository;
 import com.microservicio.ms_productos_dyc.utilities.ProductoMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +20,9 @@ public class ProductoServiceImpl implements ProductoService {
 
     @Autowired
     private ProductoRepository productoRepository;
+
+    @Autowired
+    private TipoProductoRepository tipoProductoRepository;
 
     //---------MÉTODOS GET---------//
     @Override
@@ -37,15 +44,41 @@ public class ProductoServiceImpl implements ProductoService {
     //---------MÉTODOS POST---------//
     @Override
     public ProductoDTO crear(ProductoDTO dto) {
+        // validamos que existe el tipo de producto Y está activo
+        Long idTipo = dto.getIdTipoProducto();
+        TipoProducto tipo = tipoProductoRepository.findById(idTipo)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tipo de Producto no existe: " + idTipo));
+        
+        if (tipo.getActivo() != 1) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tipo de producto está inactivo: " + idTipo);
+        }
+
+        //Convertimos y guardamos
         Producto producto = ProductoMapper.toEntity(dto);
-        return ProductoMapper.toDTO(productoRepository.save(producto));
+        producto.setTipoProducto(tipo); //nos aseguramos de forzar la entidad cargada
+        Producto saved = productoRepository.save(producto);
+        return ProductoMapper.toDTO(saved);
     }
 
 
     //---------MÉTODOS PUT---------//
     @Override
     public ProductoDTO actualizar(Long id, ProductoDTO dto) {
-        Producto existente = productoRepository.findById(id).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+        Producto existente = productoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Producto no encontrado con id: " + id));
+
+        //Validamos el tipo
+        Long idTipo = dto.getIdTipoProducto();
+        TipoProducto tipo = tipoProductoRepository.findById(idTipo)
+            .orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Tipo de Producto no existe: " + idTipo
+            ));
+        if (tipo.getActivo() != 1) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Tipo de Producto se encuentra inactivo: " +idTipo
+            );
+        }
 
         existente.setNombre(dto.getNombre());
         existente.setDescripcion(dto.getDescripcion());
@@ -54,14 +87,10 @@ public class ProductoServiceImpl implements ProductoService {
         existente.setPrecio(dto.getPrecio());
         existente.setUrlImagen(dto.getUrlImagen());
         existente.setActivo(dto.getActivo());
+        existente.setTipoProducto(tipo);
 
-        if (dto.getIdTipoProducto() != null) {
-            var tipo = new com.microservicio.ms_productos_dyc.model.entity.TipoProducto();
-            tipo.setIdTipoProducto(dto.getIdTipoProducto());
-            existente.setTipoProducto(tipo);
-        }
-
-        return ProductoMapper.toDTO(productoRepository.save(existente));
+        Producto updated = productoRepository.save(existente);
+        return ProductoMapper.toDTO(updated);
     }
 
     @Override
