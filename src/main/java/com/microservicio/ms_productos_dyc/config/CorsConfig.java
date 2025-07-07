4dcfcb619954d8568c29cf59ad7e415e3dd74d63
Jayirong <lemonsoft5555@gmail.com>
2025-07-07 19:50:56 -0400

    prueba con CORS

--- src/main/java/com/microservicio/ms_productos_dyc/config/CorsConfig.java
+++ src/main/java/com/microservicio/ms_productos_dyc/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins("http://localhost:4200", "https://uwwdb8o872.execute-api.us-east-1.amazonaws.com")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*");
             }
