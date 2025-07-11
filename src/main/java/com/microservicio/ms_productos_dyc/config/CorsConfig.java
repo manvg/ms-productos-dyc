ae7ad96dfae014e94f83a17c30c47e926f8834ff
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-07-10 23:24:51 -0400

    Actualización de configuraciones Cors: se agrega URL de producción.

--- src/main/java/com/microservicio/ms_productos_dyc/config/CorsConfig.java
+++ src/main/java/com/microservicio/ms_productos_dyc/config/CorsConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
 @Configuration
 public class CorsConfig {
 
@@ -14,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200", "https://uwwdb8o872.execute-api.us-east-1.amazonaws.com")
+                    .allowedOrigins("http://localhost:4200", "https://cortelaserdyc.cl", "https://uwwdb8o872.execute-api.us-east-1.amazonaws.com")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*");
             }
